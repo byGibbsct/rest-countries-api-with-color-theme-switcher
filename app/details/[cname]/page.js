5e70aad594701b8856fe0b6ade18f2a37a88a514
CT Gibbs <191305118+byGibbsct@users.noreply.github.com>
2024-12-14 04:59:49 -0500

    Create Panel component and style details route
    [app/details/]
    - Add Panel.js functional component
    - Add CSS classes for Panel component
    - Import and render the new component
    
    [app/page.css]
    - Add styles and layout to the Panel.js component

--- app/details/[cname]/page.js
+++ app/details/[cname]/page.js
@@ -1,7 +1,15 @@
-import "./page.css";
+import "@/app/page.css"
+
+import NavBar from "@/app/components/NavBar";
+import Panel from "./components/Panel";
 
 export default function Details() {
   return (
-    <></>
+    <>
+      <NavBar />
+      <main>
+        <Panel />
+      </main>
+    </>
   );
 }
