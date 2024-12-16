a169fa5d1a0e513ee00a3f490d9931e4353194af
CT Gibbs <191305118+byGibbsct@users.noreply.github.com>
2024-12-16 08:43:06 -0500

    Nextjs static output

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  basePath: "rest-countries-api-with-color-theme-switcher",
+  output: "export",
+  reactStrictMode: true,
+};
 
 export default nextConfig;
