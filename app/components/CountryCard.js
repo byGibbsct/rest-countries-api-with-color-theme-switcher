a93ef62db8028f46637db11e6ffcc706db94494b
CT Gibbs <191305118+byGibbsct@users.noreply.github.com>
2024-12-16 06:49:27 -0500

    Add comas to population numbers

--- app/components/CountryCard.js
+++ app/components/CountryCard.js
@@ -8,7 +8,7 @@ export default function CountryCard(props) {
         <div className="card__country-details">
           <h2 className="card__country-name">{props.name}</h2>
           <div className="card__country-info">
-            <p className="country-info__data"><span>Population:</span> {props.population}</p>
+            <p className="country-info__data"><span>Population:</span> {props.population.toLocaleString()}</p>
             <p className="country-info__data"><span>Region:</span> {props.region}</p>
             <p className="country-info__data"><span>Capital:</span> {props.capital}</p>
           </div>
